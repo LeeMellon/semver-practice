380a572911d9b288ae6bd259ed6a42cea58889f2
IMGoodrich <igoodrich@phase2technology.com>
2020-09-14 15:06:06 -0700

    refactor: 💡 update return val and test
    BREAKING CHANGE: 🧨 no

--- packages/package-two/lib/package-two.js
+++ packages/package-two/lib/package-two.js
@@ -2,6 +2,6 @@
 
 const _= require('lodash');
 
-const packageTwo = (array) => _.reduce(array, (acc, number) => acc + number + 1);
+const packageTwo = (array) => `The total is ${_.reduce(array, (acc, number) => acc + number + 1)}`;
 
 module.exports = packageTwo;
