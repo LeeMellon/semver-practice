822c73f055a2bcb11240bc486f5aba237c347cfa
IMGoodrich <igoodrich@phase2technology.com>
2020-09-14 15:10:00 -0700

    test: 💍 update to reflect changes to package-two

--- packages/package-three/__tests__/package-three.test.js
+++ packages/package-three/__tests__/package-three.test.js
@@ -3,5 +3,5 @@
 const packageThree = require('../lib/package-three');
 const packageTwo = require ('../../package-two/lib/package-two');
 test('package-three', () => {
-    expect(packageTwo(packageThree)).toEqual(139)
+    expect(packageTwo(packageThree)).toEqual('The total is 139')
 });
